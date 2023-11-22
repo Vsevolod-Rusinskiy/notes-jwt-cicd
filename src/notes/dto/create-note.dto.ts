b0199c2e3943514c840bc96e54124f1c821ac87a
vsevolod <leto9999@gmail.com>
2023-11-22 12:56:42 +0300

    add class validators

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly content: string;
 }
--- src/notes/dto/update-note.dto.ts
+++ src/notes/dto/update-note.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsOptional } from 'class-validator';
+
 export class UpdateNoteDto {
+  @IsOptional()
+  @IsString()
   readonly title?: string;
+
+  @IsOptional()
+  @IsString()
   readonly content?: string;
 }
